361a29476c3f7bb79776621ab7f6cfe31632f3e1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-08-24 19:30:19 +0900

    page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    {/* <Router basename={process.env.PUBLIC_URL}> */}
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
+    {/* </Router> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
