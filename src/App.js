48ea58a3acd2b894f90f948cae7e431869f83ce8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-08-30 13:54:49 +0900

    add build folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./Main";
 import Region from "./Region";
 import Search from "./Search";
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Router basename={process.env.PUBLIC_URL}> */}
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </HashRouter>
-    {/* </Router> */}
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
